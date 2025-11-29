770573fdaddb1ed150948c9cb27ca66a0dc73256
ruman-jagirdar <rumanjagirdar08@gmail.com>
2025-11-29 10:39:48 +0530

    Update bankw.py

--- bankw.py
+++ bankw.py
@@ -1,16 +1,19 @@
 import sys
-if len(sys.argv)==4:
-    script_name=sys.argv[0]
-    withdraw=float(sys.argv[1])
-    balance=float(sys.argv[2])
-    pin=float(sys.argv[3])
-else:
-    script_name=sys.argv[0]
-    withdraw=100
-    balance=1000
-    pin=1234
 
-if withdraw<balance:
-    print("transaction is approved")
+if len(sys.argv) == 4:
+    script_name = sys.argv[0]
+    withdraw = float(sys.argv[1])  
+    balance = float(sys.argv[2])   
+    pin = int(sys.argv[3])          
 else:
-    print("transaction is declined")
+    script_name = sys.argv[0]
+    withdraw = 100
+    balance = 1000
+    pin = 1234
+
+if withdraw <= balance:
+        balance -= withdraw
+        print("Transaction is approved")
+        print("Remaining balance:", balance)
+else:
+        print("Transaction is declined")
